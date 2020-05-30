d597247869215955c92583ef8fec14a9efd3dd90
janggalman <janggalman@gmail.com>
2020-05-30 10:25:17 +0900

    Dial Activity

---
+++ app/src/main/java/kr/tjoeun/adndroidintent_20200530/MainActivity.kt
@@ -0,0 +1,24 @@
+package kr.tjoeun.adndroidintent_20200530
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        dialBtn.setOnClickListener {
+
+            val myUri = Uri.parse("tel:01088702279")
+            val myIntent = Intent(Intent.ACTION_DIAL, myUri)
+
+            startActivity(myIntent)
+
+        }
+    }
+}
